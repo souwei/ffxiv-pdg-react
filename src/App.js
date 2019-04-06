a4cf98ef162ca8610b76953394a2b383966c547c
souwei <sou.wei.wong@gmail.com>
2019-04-06 21:05:05 +0800

    render dungeon info

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Window, TitleBar } from "react-desktop/macOs";
 import ListView from "./components/Listview";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <Window chrome height="800px" padding="10px">
+          <TitleBar title="Final Fantasy XIV Pocket Dungeon Guide" controls />
           <ListView />
-          <ListView />
-        </header>
+        </Window>
       </div>
     );
   }
--- src/components/Listview.js
+++ src/components/Listview.js
@@ -1,6 +1,100 @@
 import React from "react";
-export default class ListView extends React.Component {
+import {
+  ListView,
+  ListViewHeader,
+  ListViewFooter,
+  ListViewSection,
+  ListViewSectionHeader,
+  ListViewRow,
+  ListViewSeparator,
+  Text,
+  TitleBar,
+  Toolbar,
+  SearchField
+} from "react-desktop/macOs";
+import { dungeonsData } from "../data/dungeons";
+
+export default class CustomListView extends React.Component {
+  constructor() {
+    super();
+    this.state = { dungeonsData, selected: 1 };
+  }
+
+  handleChange = e => console.log(e.target.value);
+
   render() {
-    return <div>This is a list View </div>;
+    return (
+      <ListView
+        style={{ textAlign: "center" }}
+        background="#f1f2f4"
+        width="240"
+      >
+        <Toolbar height="43" horizontalAlignment="center">
+          <SearchField
+            placeholder="Search"
+            defaultValue=""
+            onChange={this.handleChange}
+          />
+        </Toolbar>
+
+        {this.state.dungeonsData.map(dungeonSeries => {
+          return (
+            <div>
+              <ListViewSection
+                header={this.renderSectionHeader(dungeonSeries.dungeonSeries)}
+              >
+                {dungeonSeries.dungeons.map((dungeon, idx) => {
+                  return this.renderItem(
+                    dungeonSeries.dungeonSeries + idx,
+                    dungeon.dungeonName
+                  );
+                })}
+              </ListViewSection>
+              <ListViewSeparator />
+            </div>
+          );
+        })}
+
+        <ListViewFooter>
+          <Text>github.com/souwei</Text>
+        </ListViewFooter>
+      </ListView>
+    );
+  }
+
+  renderSectionHeader(title) {
+    return <ListViewSectionHeader>{title}</ListViewSectionHeader>;
+  }
+
+  renderItem(title, info) {
+    return (
+      <ListViewRow
+        onClick={() => this.setState({ selected: title })}
+        background={this.state.selected === title ? "#d8dadc" : null}
+      >
+        <svg
+          x="0px"
+          y="0px"
+          width="18"
+          height="12"
+          viewBox="0 0 18 12"
+          style={{ marginRight: "6px" }}
+        >
+          <path
+            fill="#727476"
+            d="M13.2,0H4.9L0,6.8v3.7C0,11.3,0.7,12,1.5,12h15
+    c0.8,0,1.5-0.7,1.5-1.5V6.8L13.2,0z M13.8,6.8L12.3,9L5.9,9L4.2,6.8l-3.1,0l4.2-6h7.4l4.2,6L13.8,6.8z"
+          />
+          <polygon
+            fill="#C9CBCD"
+            points="13.8,6.8 12.3,9 5.9,9 4.2,6.8 1.2,6.7 5.4,0.8 12.8,0.8
+    17,6.7 "
+          />
+        </svg>
+        <Text color="#414141" size="13">
+          {info}
+        </Text>
+      </ListViewRow>
+    );
   }
 }
