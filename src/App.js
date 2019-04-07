6d094c70d5a23b081567c0cb2c9602464211cc4e
souwei <sou.wei.wong@gmail.com>
2019-04-07 14:46:21 +0800

    added ionic core components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import ListView from "./components/Listview";
+
+import {
+  IonCard,
+  IonCardTitle,
+  IonLabel,
+  IonItem,
+  IonList
+} from "@ionic/react";
+import { dungeonsData } from "./data/dungeons";
+import msq from "./assets/msq.png";
+import arr from "./assets/arr.png";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dungeonsData
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <ListView />
-          <ListView />
-        </header>
+      <div>
+        {this.state.dungeonsData.map(series => {
+          return (
+            <IonCard>
+              <IonCardTitle>
+                <img
+                  src={arr}
+                  alt="arr"
+                  style={{ height: "auto", width: 300, paddingLeft: 0 }}
+                />
+              </IonCardTitle>
+              <IonList>
+                {series.dungeons.map((dungeon, idx) => {
+                  return (
+                    <IonItem>
+                      <img
+                        style={{
+                          height: "auto",
+                          width: 30,
+                          paddingLeft: 0,
+                          verticalAlign: "middle"
+                        }}
+                        src={msq}
+                        alt="msq"
+                      />
+                      <span class="h2">{dungeon.dungeonName} </span>
+                    </IonItem>
+                  );
+                })}
+              </IonList>
+            </IonCard>
+          );
+        })}
       </div>
     );
   }
