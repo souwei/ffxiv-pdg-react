d6beedd3d24ee714ff0620785e9e98d19c99da2c
souwei <sou.wei.wong@gmail.com>
2019-04-07 15:21:16 +0800

    clean up and moved list to components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-
-import {
-  IonCard,
-  IonCardTitle,
-  IonLabel,
-  IonItem,
-  IonList
-} from "@ionic/react";
 import { dungeonsData } from "./data/dungeons";
-import msq from "./assets/msq.png";
-import arr from "./assets/arr.png";
+import { ListView } from "./components/Listview";
 
 class App extends Component {
   constructor(props) {
@@ -24,34 +15,10 @@ class App extends Component {
       <div>
         {this.state.dungeonsData.map(series => {
           return (
-            <IonCard>
-              <IonCardTitle>
-                <img
-                  src={arr}
-                  alt="arr"
-                  style={{ height: "auto", width: 300, paddingLeft: 0 }}
-                />
-              </IonCardTitle>
-              <IonList>
-                {series.dungeons.map((dungeon, idx) => {
-                  return (
-                    <IonItem>
-                      <img
-                        style={{
-                          height: "auto",
-                          width: 30,
-                          paddingLeft: 0,
-                          verticalAlign: "middle"
-                        }}
-                        src={msq}
-                        alt="msq"
-                      />
-                      <span class="h2">{dungeon.dungeonName} </span>
-                    </IonItem>
-                  );
-                })}
-              </IonList>
-            </IonCard>
+            <ListView
+              ListTitle={series.dungeonSeries}
+              ListData={series.dungeons}
+            />
           );
         })}
       </div>
--- src/components/Listview.js
+++ src/components/Listview.js
@@ -1,6 +1,84 @@
 import React from "react";
-export default class ListView extends React.Component {
-  render() {
-    return <div>This is a list View </div>;
+import { PropTypes } from "prop-types";
+import {
+  IonCard,
+  IonCardTitle,
+  IonItem,
+  IonList
+} from "@ionic/react";
+import msq from "../assets/msq.png";
+import arr from "../assets/arr.png";
+import hw from "../assets/hw.png";
+import sb from "../assets/sb.png";
+
+const renderDungeonSeries = seriesName => {
+  switch (seriesName) {
+    case "A Realm Reborn":
+      return (
+        <IonCardTitle>
+          <img
+            src={arr}
+            alt={seriesName}
+            style={{ height: "auto", width: 300, paddingLeft: 0 }}
+          />
+        </IonCardTitle>
+      );
+
+    case "Heavensward":
+      return (
+        <IonCardTitle>
+          <img
+            src={hw}
+            alt={seriesName}
+            style={{ height: "auto", width: 300, paddingLeft: 0 }}
+          />
+        </IonCardTitle>
+      );
+
+    default:
+      return (
+        <IonCardTitle>
+          <img
+            src={sb}
+            alt={seriesName}
+            style={{ height: "auto", width: 300, paddingLeft: 0 }}
+          />
+        </IonCardTitle>
+      );
   }
-}
+};
+
+export const ListView = ({ ListTitle, ListData }) => {
+  return (
+    <div>
+      <IonCard>
+        {renderDungeonSeries(ListTitle)}
+        <IonList>
+          {ListData.map((dungeon, idx) => {
+            return (
+              <IonItem>
+                <img
+                  style={{
+                    height: "auto",
+                    width: 30,
+                    paddingLeft: 0,
+                    verticalAlign: "middle"
+                  }}
+                  src={msq}
+                  alt="msq"
+                />
+                <span class="h2">{dungeon.dungeonName} </span>
+              </IonItem>
+            );
+          })}
+        </IonList>
+      </IonCard>
+      ;
+    </div>
+  );
+};
+
+ListView.propTypes = {
+  ListTitle: PropTypes.string.isRequired,
+  ListData: PropTypes.array.isRequired
+};
